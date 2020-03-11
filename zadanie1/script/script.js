a2468b8517c8fa749153dfd8a0944cefd86f2d4e
Sergey <sergey_shelepov@list.ru>
2020-03-11 21:49:20 +0300

    сделал первое дз

--- zadanie1/script/script.js
+++ zadanie1/script/script.js
@@ -1,1 +1,39 @@
 'use strict';
+
+let money,
+  time,
+  rash1,
+  rash2,
+  budgetOneDay,
+  expenses,
+  optionalExpenses,
+  appData;
+
+money = +prompt("Ваш бюджет на месяц?", "");
+time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+
+rash1 = prompt("Введите обязательную статью расходов в этом месяце", "");
+priceRash1 = +prompt("Во сколько обойдется?", "");
+
+rash2 = prompt("Введите обязательную статью расходов в этом месяце", "");
+priceRash2 = +prompt("Во сколько обойдется?", "");
+
+budgetOneDay = alert("Бюджет на один день= " + money / 30);
+
+expenses = {
+  priceRash1: priceRash2,
+};
+
+optionalExpenses = {
+
+};
+
+appData = {
+  budget: money,
+  timeData: time,
+  rash: expenses,
+  optRash: optionalExpenses,
+  income: "",
+  savings: false,
+};
