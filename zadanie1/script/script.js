1c35216d832e5515ae5c0e5bf2e70923161c7efa
Sergey <sergey_shelepov@list.ru>
2020-03-11 22:02:37 +0300

    dz1 fix

--- zadanie1/script/script.js
+++ zadanie1/script/script.js
@@ -1,39 +1,23 @@
 'use strict';
 
-let money,
-  time,
-  rash1,
-  rash2,
-  budgetOneDay,
-  expenses,
-  optionalExpenses,
-  appData;
+let money = +prompt("Ваш бюджет на месяц?", ""),
+  time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
-money = +prompt("Ваш бюджет на месяц?", "");
-time = prompt("Введите дату в формате YYYY-MM-DD", "");
-
-
-rash1 = prompt("Введите обязательную статью расходов в этом месяце", "");
-priceRash1 = +prompt("Во сколько обойдется?", "");
-
-rash2 = prompt("Введите обязательную статью расходов в этом месяце", "");
-priceRash2 = +prompt("Во сколько обойдется?", "");
-
-budgetOneDay = alert("Бюджет на один день= " + money / 30);
-
-expenses = {
-  priceRash1: priceRash2,
-};
-
-optionalExpenses = {
-
-};
-
-appData = {
+let appData = {
   budget: money,
   timeData: time,
-  rash: expenses,
-  optRash: optionalExpenses,
-  income: "",
+  expenses: {},
+  optionalExpenses: {},
+  income: [],
   savings: false,
 };
+
+let rash1 = prompt("Введите обязательную статью расходов в этом месяце", ""),
+  priceRash1 = prompt("Во сколько обойдется?", ""),
+  rash2 = prompt("Введите обязательную статью расходов в этом месяце", ""),
+  priceRash2 = prompt("Во сколько обойдется?", "");
+
+appData.expenses.rash1 = priceRash1;
+appData.expenses.rash2 = priceRash2;
+
+budgetOneDay = alert("Бюджет на один день= " + appData.budget / 30);
