2d862f7304c97d04d59d31a9c8b40e736b17348a
Sergey <sergey_shelepov@list.ru>
2020-03-15 21:39:08 +0300

    второе домашнее задание

--- vvodnaia/app/js/script.js
+++ vvodnaia/app/js/script.js
@@ -1,31 +1,70 @@
 'use strict';
 
-let num = 50;
-if (num < 49) {
-  console.log('neverno');
-} else if (num > 100) {
-  console.log('mnogo');
-} else {
-  console.log('verno');
 
-}
+// if (num < 49) {
+//   console.log('neverno');
+// } else if (num > 100) {
+//   console.log('mnogo');
+// } else {
+//   console.log('verno');
 
-(num == 50) ? console.log('verno'): console.log('neverno');
+// }
 
-switch (num) {
-  case num < 49:
-    console.log('neverno');
-    break;
-  case num > 100:
-    console.log('mnogo');
-    break;
-  case num > 80:
-    console.log('vse ese mnogo');
-    break;
-  case 50:
-    console.log('verno');
-    break;
-  default:
-    console.log('что то пошло не так');
-    break;
-}
+// (num == 50) ? console.log('verno') : console.log('neverno');
+
+// switch (num) {
+//   case num < 49:
+//     console.log('neverno');
+//     break;
+//   case num > 100:
+//     console.log('mnogo');
+//     break;
+//   case num > 80:
+//     console.log('vse ese mnogo');
+//     break;
+//   case 50:
+//     console.log('verno');
+//     break;
+//   default:
+//     console.log('что то пошло не так');
+//     break;
+// }
+
+
+// let num = 50;
+// // while (num < 55) {
+// //   console.log(num);
+// //   num++;
+// // }
+
+// do {
+//   console.log(num);
+//   num++;
+// }
+// while (num < 55);
+
+// for (let i = 1; i < 8; i++) {
+//   if (i == 6) {
+//     continue
+//   }
+//   console.log(i);
+// }
+
+// let y = 1;
+// let x = y = 2;
+// alert(null || 2 && 3 || 4);
+// console.log(2 && 1 && null && 0 && undefined);
+// console.log(!!(a && b));
+
+
+// let a1 = [1, 2, 3];
+// let b1 = [1, 2, 3];
+
+
+// if (a1 == b1) {
+//   console.log("dthyj");
+// } else {
+//   console.log("neverno");
+// }
+alert(+"Infinity");
+console.log(0 || "" || 2 || undefined || true || falsе);
